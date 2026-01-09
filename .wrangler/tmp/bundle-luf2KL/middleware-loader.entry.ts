380fb61a3510912dc4dd814076a317dd5f94f2f9
biwasbhandari <biwas2059@gmail.com>
2026-01-09 11:27:22 +0545

    add project name

--- .wrangler/tmp/bundle-luf2KL/middleware-loader.entry.ts
+++ .wrangler/tmp/bundle-luf2KL/middleware-loader.entry.ts
@@ -3,12 +3,12 @@
 // export dynamically through wrangler, or we can potentially let users directly
 // add them as a sort of "plugin" system.
 
-import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/biwas/claudex402/landing-page/.wrangler/tmp/bundle-c7KAm9/middleware-insertion-facade.js";
+import ENTRY, { __INTERNAL_WRANGLER_MIDDLEWARE__ } from "/Users/biwas/claudex402/landing-page/.wrangler/tmp/bundle-luf2KL/middleware-insertion-facade.js";
 import { __facade_invoke__, __facade_register__, Dispatcher } from "/Users/biwas/claudex402/landing-page/node_modules/wrangler/templates/middleware/common.ts";
-import type { WorkerEntrypointConstructor } from "/Users/biwas/claudex402/landing-page/.wrangler/tmp/bundle-c7KAm9/middleware-insertion-facade.js";
+import type { WorkerEntrypointConstructor } from "/Users/biwas/claudex402/landing-page/.wrangler/tmp/bundle-luf2KL/middleware-insertion-facade.js";
 
 // Preserve all the exports from the worker
-export * from "/Users/biwas/claudex402/landing-page/.wrangler/tmp/bundle-c7KAm9/middleware-insertion-facade.js";
+export * from "/Users/biwas/claudex402/landing-page/.wrangler/tmp/bundle-luf2KL/middleware-insertion-facade.js";
 
 class __Facade_ScheduledController__ implements ScheduledController {
 	readonly #noRetry: ScheduledController["noRetry"];
